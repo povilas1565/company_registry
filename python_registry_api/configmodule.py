5afeaa0dcc5746c20d28aa94cfc10d3f84c7459b
L0rd <headood007@gmail.com>
2023-04-13 07:05:50 +0300

    removed postgreSQL

--- python_registry_api/configmodule.py
+++ python_registry_api/configmodule.py
@@ -5,13 +5,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@db:5432/{os.getenv('POSTGRES_DB')}"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "data.sqlite")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class WebTestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "test_data.sqlite")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "data.sqlite")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
