010d551bdd0ad44eec5dcad6bf26c345f4f8292f
L0rd <headood007@gmail.com>
2023-04-14 14:06:30 +0300

    fix api ur

--- python_registry_api/startup.sh
+++ python_registry_api/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 flask db upgrade
-gunicorn -w 4 -b 0.0.0.0:5000 'app:create_app()'
+gunicorn -w 4 -b 0.0.0.0:5000 'app:create_app()' --error-logfile "-" --enable-stdio-inheritance --reload --log-level "debug"
